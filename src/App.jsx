180de0c50570a3cb17941a1108732e558eb9e0ae
Kshitij <kshitij.tanwar2002@gmail.com>
2025-03-28 03:40:12 +0530

    chore: created login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,12 @@
+import Login from "./pages/Login";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 const App = () => {
-    return <div>App</div>;
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Login />} />
+            </Routes>
+        </Router>
+    );
 };
 export default App;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,72 @@
+import { useForm } from "react-hook-form";
+const Login = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+    const handleLogin = (data) => {
+        console.log(data);
+    };
+    return (
+        <section className="bg-black h-screen flex items-center justify-center p-4">
+            <div className="border border-neutral-500 w-full sm:max-w-md p-5 rounded-lg">
+                <h1 className="text-4xl text-white font-bold">Sign in</h1>
+                <form
+                    className="flex flex-col gap-4 mt-5"
+                    onSubmit={handleSubmit(handleLogin)}
+                >
+                    <div className="flex flex-col gap-2">
+                        <label htmlFor="email" className="text-neutral-400">
+                            Email
+                        </label>
+                        <input
+                            {...register("email", { required: true })}
+                            id="email"
+                            type="text"
+                            className="border border-neutral-600 rounded-md px-4 py-2 text-white hover:border-neutral-400 transition-colors"
+                            placeholder="your@email.com"
+                        />
+                        {errors.email && (
+                            <p className="text-red-500 text-sm text-right">
+                                Email is required
+                            </p>
+                        )}
+                    </div>
+                    <div className="flex flex-col gap-2">
+                        <label htmlFor="email" className="text-neutral-400">
+                            Password
+                        </label>
+                        <input
+                            {...register("password", { required: true })}
+                            type="password"
+                            className="border border-neutral-600 rounded-md px-4 py-2 text-white hover:border-neutral-400 transition-colors"
+                            placeholder="••••••••"
+                        />
+                        {errors.password && (
+                            <p className="text-red-500 text-sm text-right">
+                                Password is required
+                            </p>
+                        )}
+                    </div>
+                    <div className="w-fit">
+                        <label className="flex items-center gap-2 text-neutral-400 hover:underline cursor-pointer">
+                            <input
+                                type="checkbox"
+                                className="accent-neutral-500"
+                            />
+                            Remember me
+                        </label>
+                    </div>
+                    <button
+                        type="submit"
+                        className="px-4 py-2 bg-white rounded-md hover:bg-neutral-200 transition-colors text-black font-semibold cursor-pointer"
+                    >
+                        Submit
+                    </button>
+                </form>
+            </div>
+        </section>
+    );
+};
+export default Login;
